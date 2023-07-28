a897eff6ad38c4a3762eb0b26575542067627f71
Adrian Siekierka <kontakt@asie.pl>
2023-07-28 17:02:29 +0200

    relicense as LGPLv3

--- src/main/java/pl/asie/foamfix/coremod/patches/jei/SearchTreeJEIPatchGlue.java
+++ src/main/java/pl/asie/foamfix/coremod/patches/jei/SearchTreeJEIPatchGlue.java
@@ -1,31 +1,21 @@
-/*
+/**
  * Copyright (C) 2016, 2017, 2018, 2019, 2020, 2021 Adrian Siekierka
  *
  * This file is part of FoamFix.
  *
  * FoamFix is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
+ * it under the terms of the GNU Lesser General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
  * FoamFix is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ * GNU Lesser General Public License for more details.
  *
- * You should have received a copy of the GNU General Public License
+ * You should have received a copy of the GNU Lesser General Public License
  * along with FoamFix.  If not, see <http://www.gnu.org/licenses/>.
- *
- * Additional permission under GNU GPL version 3 section 7
- *
- * If you modify this Program, or any covered work, by linking or
- * combining it with the Minecraft game engine, the Mojang Launchwrapper,
- * the Mojang AuthLib and the Minecraft Realms library (and/or modified
- * versions of said software), containing parts covered by the terms of
- * their respective licenses, the licensors of this Program grant you
- * additional permission to convey the resulting work.
  */
-
 package pl.asie.foamfix.coremod.patches.jei;
 
 import net.minecraftforge.fml.common.Loader;
